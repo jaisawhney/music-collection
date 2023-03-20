3549ba5f0ab81593ba714b4ab681e66ee34bed8d
Jai Sawhney <jai.sawhney1@gmail.com>
2023-03-20 11:40:45 -0700

    Add header

--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -1,8 +1,31 @@
+import { NavLink } from 'react-router-dom';
+import { clsx } from 'clsx';
+
 export default function Header() {
 
+
+    const test = ({ isActive }: { isActive: boolean }) => {
+        return clsx(
+            'p-1 font-bold uppercase',
+            isActive && 'bottom-pill',
+        );
+    };
+
     return (
-        <div className={'p-2 w-full bg-slate-100 text-center'}>
-            <p className={'uppercase'}>Nav buttons here</p>
+        <div className={clsx(
+            'p-4 w-full text-center flex justify-center gap-5',
+            'sm:gap-24',
+        )}>
+            <NavLink to={`/`} className={test}>
+                <p>Home</p>
+            </NavLink>
+            <NavLink to={`/library`} className={test}>
+                <p>Library</p>
+            </NavLink>
+
+            <NavLink to={`/settings`} className={test}>
+                <p>Settings</p>
+            </NavLink>
         </div>
     );
 }
